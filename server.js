0e745d865a90d878794552dd84239639f2cba384
Ashar Ardianto <ashardian.to21@gmail.com>
2021-11-25 11:09:06 +0700

    commit on 'main'

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
+app.get('/chapter-3', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
