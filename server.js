a8b69a9dd9ef67a4c3421b07e05b5be56e85cbb5
Ashar Ardianto <ashardian.to21@gmail.com>
2021-11-25 11:34:13 +0700

    commit on 'main'

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/chapter-3', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/chapter-3/index.html'));
 });
+app.get('/chapter-4', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/chapter-4/index.html'));
+});
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
